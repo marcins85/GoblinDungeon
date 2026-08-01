233051cbf5951c2360bae61491a5e992e54f1cac
marcins85 <stassimon@gmail.com>
2026-08-01 18:28:52 +0200

    Adding simple test pawn class

---
+++ Source/GoblinDungeon/Public/Pawns/Bird.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include <Components/CapsuleComponent.h>
+#include "Bird.generated.h"
+
+UCLASS()
+class GOBLINDUNGEON_API ABird : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	ABird();
+
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UCapsuleComponent* Capsule;
+};
